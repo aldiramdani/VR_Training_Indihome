315e90f7ca975a29696c14601c80c54076e4e066
Aldi Ramdani <aldiramdani8@gmail.com>
2020-02-15 16:34:32 +0700

    Tambah Tampilan Profilling
    Tambah Tampilan Profilling

---
+++ Assets/Script/Profiling/Profilling.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Profilling : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Button btn_mulai;
+    public InputField inputNama,inputNik;
+    public Dropdown drpTempatKerja;
+    string nama, nik, lok_kerja;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void getProfilling()
+    {
+        nama = inputNama.text;
+        nik = inputNik.text;
+        lok_kerja = drpTempatKerja.itemText.text;
+        if (nik == PlayerPrefs.GetString("Nik User"))
+        {
+            SSTools.ShowMessage("Nik Sudah Terdaftar", SSTools.Position.bottom, SSTools.Time.threeSecond);
+        }
+        else
+        {
+            PlayerPrefs.SetString("Nik User", nik);
+            PlayerPrefs.SetString("Nama" + nik, nama);
+            PlayerPrefs.SetString("Lok Kerja" + nik, lok_kerja);
+        }
+        Debug.Log(nama + nik + lok_kerja);
+    }
+
+    public void checkOtherUser()
+    {
+        
+    }
+}
